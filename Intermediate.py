3450b13a8206c6535be44432776acacddaed396d
Anish <anishfeb2006@gmail.com>
2025-03-21 09:28:02 +0530

    Updated class and objects  Intermediate.py

--- Intermediate.py
+++ Intermediate.py
@@ -38,3 +38,53 @@ tri=triangle()
 x=tri.base(42)
 y=tri.height(35)
 calculate_area()
+
+
+#Class and objects
+class animal:
+    def omnivorous(self):
+        print("This animal belongs to omnivorous species")
+class dog(animal):
+    def __init__(self,bark,breed):
+        print("This is a dog")
+        self.bark=bark
+        self.breed=breed
+    def barks(self):
+     print("Dog will bark")
+class cat(animal):
+    print("This is a cat")
+    def __init__(self,meow,breed):
+        self.meow=meow
+        self.breed=breed
+    def moos(self):
+        print("Cat meows")
+D=dog("bark","golden retriever")
+D.barks()
+D.omnivorous()
+c=cat("meow","burmese")
+c.moos()
+c.omnivorous()
+
+#class and objects for aml department
+class college:
+    def general_purpose(self):
+        print("It provides knowledge to students")
+class Department(college):
+    def __init__(self,name,code):
+        print("You belong to aml department")
+        self.name=name
+        self.code=code
+    def specific_Department(self):
+        print("This department provides knowledge about artificial intelligence and machine learning")
+c=Department("Aml",3123)
+c.general_purpose()
+c.specific_Department()
+
+#Iterators
+class channels:
+    def __init__(self):
+        self.channels=["Ngo","Espn","Ssn","Bbc"]
+        self.index=-1
+    def __iter__(self):
+        self.index=1
+        
